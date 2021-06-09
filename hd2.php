83b66d732ae6d11b67cc15c5a5e4d4319e6e193f
Ray Hollister <github@rayhollister.com>
2021-06-09 14:50:49 -0400

    First Commit, basic proof of concept
    Triton default audio players

---
+++ hd2.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html>
+
+<head>
+    <title>Triton Media Player Test</title>
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+</head>
+
+
+<body style="background: #f5f5f5; text-align:center;">
+<p>The player below should show the song title and artist if the stream is receiving the metadata.</p>
+
+    <div style="margin:0 auto;max-width:600px;">
+        <h3>WJCT-HD2 Classical24</h3>
+        <div class="player-container2" style="background:#f5f5f5;">
+            <td-player id="td-player2" type="bar" autostart="false" defaultcoverart="//player.listenlive.co/templates/StandardPlayerV4/webroot/img/default-cover-art.png" station="WJCT_HD2">
+            </td-player>
+
+        </div>
+
+        <!-- <h3>WJCT-HD3 Anthology</h3>
+
+        <div class="player-container3" style="background:#f5f5f5;">
+            <td-player id="td-player3" type="bar" autostart="false" defaultcoverart="//player.listenlive.co/templates/StandardPlayerV4/webroot/img/default-cover-art.png" station="WJCTHD3">
+            </td-player>
+
+        </div> -->
+
+        <!-- <h3>WJCT-HD4 Electro Lounge Radio</h3>
+        <div class="player-container4" style="background:#f5f5f5;">
+            <td-player id="td-player4" type="bar" autostart="false" defaultcoverart="//player.listenlive.co/templates/StandardPlayerV4/webroot/img/default-cover-art.png" station="WJCT_HD4">
+            </td-player>
+
+        </div> -->
+
+
+    </div>
+
+
+    <script src="//widgets.listenlive.co/1.0/tdwidgets.min.js" platform="prod"></script>
+
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+
+<head>
+    <title>Triton Media Player Test</title>
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+</head>
+
+
+<body style="background: #f5f5f5; text-align:center;">
+    <div style="margin:0 auto;max-width:500px;">
+    <h1>Triton Media Player <br/>Proof of Concept</h1>
+        <h3><a href="hd2.php">WJCT-HD2 Classical24</a></h3>
+        <h3><a href="hd3.php">WJCT-HD3 Anthology</a></h3>
+        <h3><a href="hd4.php">WJCT-HD4 Electro Lounge Radio</a></h3>
+    </div>
+
+
+    <script src="//widgets.listenlive.co/1.0/tdwidgets.min.js" platform="prod"></script>
+
+
+</body>
+
+</html>
